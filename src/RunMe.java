001cb04896f5a7c69c244ba769aaeb210551ea72
Abhi2009CU <suresh.abhinav09@gmail.com>
2026-01-28 10:21:52 -0800

    fix RunMe.java

--- src/RunMe.java
+++ src/RunMe.java
@@ -3,7 +3,7 @@ import core.DisplayWindow;
 public class RunMe {
     public static void main(String[] args) {
         // --== Load an image to filter ==--
-        DisplayWindow.showFor("images/img.png", 768, 1024, "CardsFilter");
+        DisplayWindow.showFor("images/img.png", 768, 1024, "CardFilter");
 
         // --== Determine your input interactively with menus ==--
 //        DisplayWindow.getInputInteractively(800,600);
